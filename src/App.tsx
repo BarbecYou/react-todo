84975c83acb077d713308dc1b60018d764331d45
Füredi Dániel <daniel.furedi03@gmail.com>
2023-02-12 20:58:23 +0100

    input for todos layout done, needs style and backend functioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 
+// custom components
+import CustomForm from './assets/components/CustomForm'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-
+    <div className="container">
+      <header>
+        <h1>my task list</h1>
+      </header>
+      <CustomForm />
     </div>
   )
 }
--- src/assets/components/CustomForm.tsx
+++ src/assets/components/CustomForm.tsx
@@ -1,8 +1,37 @@
-export const CustomForm = () => {
+import { FormEvent, useState } from "react";
 
+// custom icons
+import { PlusCircleIcon } from '@heroicons/react/24/solid';
+
+const CustomForm = () => {
+    const [task, setTask] = useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault;
+        console.log(e);
+    }
 
     return (
-        <div>CustomForm</div>
+        <form
+            className="form"
+            onSubmit={handleSubmit}>
+            <div className="formGrid">
+                <label htmlFor="taskInput">enter task</label>
+                <button type="submit" id="submitBtn">
+                    <PlusCircleIcon />
+                </button>
+                <input
+                    type="text"
+                    id="taskInput"
+                    className="input"
+                    value={task}
+                    onInput={(e: FormEvent<HTMLInputElement>) => { setTask((e.target as HTMLInputElement).value) }}
+                    required
+                    autoFocus
+                    maxLength={60}
+                />
+            </div>
+        </form>
     )
 }
-
+export default CustomForm;
